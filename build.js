a043a1663a0f648e52e71293b287cf17b3e2fd3a
bdoshi <bdoshi@cisco.com>
2025-06-15 22:38:27 -0700

    missed file from build

--- build.js
+++ build.js
@@ -148,7 +148,12 @@ class JABBuilder {
     async processHTMLFiles(cssMap, jsMap) {
         console.log('📄 Processing HTML files...');
         
-        const htmlFiles = ['index.html', 'story.html','timeline_duration-junior.html', 'timeline_duration-senior.html'];
+        const htmlFiles = [
+            'index.html', 
+            'story.html',
+            'timeline_duration-junior.html', 
+            'timeline_duration-senior.html',
+            'timeline-k-to-m.html'];
         
         for (const file of htmlFiles) {
             const srcPath = path.join(this.sourceDir, file);
